d6d46da992a218be2b24fe3a3993606e25b39798
m0rp3x <yurash612@gmail.com>
2022-11-27 14:10:06 +0300

    Initial commit

---
+++ quiz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using quiz;
+
+namespace ConsoleApp12
+{
+    internal class Bebra
+    {
+        public static  void Main(string[] args)
+        {
+            int ans = 0;
+            List<Questions> xyi = new List<Questions>
+            {
+                new Questions(1,"Какое название имеют конспирологические теории по поводу высадки NASA на луне?","лунный заговор"),
+                new Questions(2,"Последння буква в Алфавите","Я"),
+                new Questions(3,"Сколько лет длилось крепостное право?","212"),
+                new Questions(4,"Улукоморье","Дуб зелёный"),
+                
+            };
+
+            List<string?> answer = new List<string?>();
+                
+            foreach (var i in xyi )
+            {
+                Console.WriteLine($"{i.Question}");
+                Console.WriteLine();
+                  answer.Add(Console.ReadLine());
+                  if (answer.Contains(i.Answer))
+                  {
+                      ans += 1;
+                  }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Вы ответили правильно на {ans} что является { ans * 100 / xyi.Count}%");
+            
+            
+            File.WriteAllText(@"C:\Users\koval\RiderProjects\quiz\quiz\quiz.json", JsonSerializer.Serialize(xyi));
+            File.WriteAllText(@"C:\Users\koval\RiderProjects\quiz\quiz\answer.json", JsonSerializer.Serialize(answer));
+            
+       
+        }
+        
+        
+    }
+    
+}
---
+++ quiz/Question.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp12;
+
+namespace quiz
+{
+    public class  Questions
+    {
+            
+
+
+        public Questions  (int id, String question, string answer)
+        {
+            Id = id;
+            Question = question;
+            Answer = answer;
+        }
+        
+        public string GetQuestion() => Question;
+        public string GetAnswer() => Answer;
+        
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public int Id { get; set; }
+
+
+       
+    }
+}
+ 
